f3726318a34fd87d5424d6e28b7cae8d7f795468
sesvis <105297500+sesvisuals@users.noreply.github.com>
2022-11-02 21:09:01 -0500

    19.2 upload

--- Copy of 19.1-activities/03_Using_jQuery/unsolved/js/index.js
+++
@@ -1,5 +0,0 @@
-// Use jQuery to target the <h1> tag and make it red.
-
-// Use jQuery to target the blue class and make it blue.
-
-// Use jQuery to target the addMe ID and change its HTML to Hello.
